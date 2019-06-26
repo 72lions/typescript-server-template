30e8ddeddb1a76f40f098597b8feaa47b3b1fb46
Theodoros Tsiridis <thodoris@spotify.com>
2019-06-26 15:35:43 +0200

    Initial setup

---
+++ src/routes/healthcheck.route.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express'
+
+const healthcheckRouter = express.Router()
+
+healthcheckRouter.get('/_ah/health', async (req: Request, res: Response) => {
+  res.status(200).send('OK')
+})
+
+healthcheckRouter.get('/_ah/liveness', async (req: Request, res: Response) => {
+  res.status(200).send('OK')
+})
+
+export default healthcheckRouter
---
+++ src/server.ts
@@ -0,0 +1,64 @@
+import compression from 'compression'
+import * as dotenv from 'dotenv'
+import express, {
+  Application,
+  ErrorRequestHandler,
+  Request,
+  RequestHandler,
+  Response,
+} from 'express'
+import log4js from 'log4js'
+import morgan from 'morgan'
+import { Z_DEFAULT_COMPRESSION } from 'zlib'
+import healthcheckRouter from './routes/healthcheck.route'
+
+dotenv.config()
+
+log4js.configure({
+  appenders: { out: { type: 'stdout' } },
+  categories: { default: { appenders: ['out'], level: 'all' } },
+})
+const log = log4js.getLogger('server.ts')
+
+const bootstrap = async () => {
+  const app: Application = express()
+  app.set('trust proxy', true)
+  app.use(morgan('combined'))
+  app.use(compression({ level: Z_DEFAULT_COMPRESSION }))
+
+  /**
+   * Your routes and middlewares go here
+   */
+
+  app.use('/', healthcheckRouter)
+
+  /**
+   * End of your routes and middlewares go here
+   */
+  app.use(
+    (
+      err: ErrorRequestHandler,
+      req: Request,
+      res: Response,
+      next: RequestHandler,
+    ) => {
+      log.error(err)
+    },
+  )
+
+  const port = process.env.PORT || 8080
+  const isProd = process.env.NODE_ENV === 'production'
+
+  log.info(`isProduction: ${isProd}`)
+
+  app.listen(port, () => {
+    log.info(`Server started on port ${port}!`)
+  })
+}
+
+const time = Date.now()
+bootstrap()
+  .then(res => {
+    log.info(`Took ${Date.now() - time}ms to bootstrap the server`)
+  })
+  .catch(e => log.error(e))
